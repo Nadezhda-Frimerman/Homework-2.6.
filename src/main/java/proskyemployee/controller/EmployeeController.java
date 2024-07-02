d192dffac2db9cace41a12e7affcc97d42ef9697
Nadine273 <tomns@mail.ru>
2024-07-02 21:37:13 +0200

    v

--- src/main/java/proskyemployee/controller/EmployeeController.java
+++ src/main/java/proskyemployee/controller/EmployeeController.java
@@ -1,7 +1,10 @@
 package proskyemployee.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import proskyemployee.service.EmployeeService;
 
 @RestController
 public class EmployeeController {
+    private EmployeeService employeeService;
+
 }
--- src/main/java/proskyemployee/service/EmployeeService.java
+++ src/main/java/proskyemployee/service/EmployeeService.java
@@ -1,16 +1,42 @@
 package proskyemployee.service;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import proskyemployee.Employee;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 @Service
 public class EmployeeService {
-    private static List employees;
-    List <Employee> employees = new ArrayList<Employee>();
-    private static int employeeAmount;
+    private int employeeAmount;
+    Employee tom=new Employee("Tom","Smith");
+    Employee don=new Employee("Don","Adams");
+    Employee ann =new Employee("Ann","Henson");
+    Employee kate=new Employee("Kate","Tomson");
+    private List<Employee> employees = new ArrayList<Employee>(Arrays.asList(tom,don,ann,kate));
+
+
+
+    public EmployeeService(int employeeAmount,List employees){
+        this.employeeAmount=employeeAmount;
+        this.employees= employees;
+    }
+    public void addEmployee(String firstName, String lastname){
+        Employee e =new Employee(firstName,lastname);
+        if (employees.size()>=employeeAmount){
+
+
+        }
+        for (int i = 0; i < employees.size(); i++) {
+            if (employees.get(i)==null){
+                employees.add(new Employee(firstName, lastname));
+            }
+        }
+    }
+
 
 }
